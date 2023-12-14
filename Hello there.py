a4cac7bede79836d3cc0ff4df13e257c7c074176
Mehmet Baran Ozkutuk <64807009+MOzkutuk@users.noreply.github.com>
2023-12-14 14:52:30 +0100

    Add files via upload

--- Hello there.py
+++ Hello there.py
@@ -1,1 +1,10 @@
-print("Hello there")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hey there'
+
+if __name__ == '__main__':
+    app.run(debug=True)
